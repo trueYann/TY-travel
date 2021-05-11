b5855142d947d9b95d59ebaebcc6e50eb89f1425
trueYann <trueyann@qq.com>
2021-05-11 11:14:05 +0800

    细节优化

--- react-travel/src/components/index.ts
+++ react-travel/src/components/index.ts
@@ -4,7 +4,6 @@ export * from './carousel'
 export * from './sideMenu'
 export * from './productCollection'
 export * from './businessPartners'
-<<<<<<< HEAD
 export * from './productIntro'
 export * from './productComments'
 export * from './filter'
@@ -12,12 +11,3 @@ export * from './productList'
 export * from './paymentCard'
 export * from './paymentForm'
 export * from './checkOutCard'
-=======
-export * from "./productIntro"
-export * from "./productComments"
-export * from "./filter"
-export * from './productList'
-export * from './paymentCard'
-export * from './paymentForm'
-export * from './checkOutCard'
->>>>>>> 0185803b3109c0c839bfdeecb21dcc88507afbd1
--- react-travel/src/redux/hooks.ts
+++ react-travel/src/redux/hooks.ts
@@ -1,16 +1,7 @@
 import {
-<<<<<<< HEAD
 	useSelector as useReduxSelector,
 	TypedUseSelectorHook,
 } from 'react-redux'
 import { RootState } from './store'
 
 export const useSelector: TypedUseSelectorHook<RootState> = useReduxSelector
-=======
-  useSelector as useReduxSelector,
-  TypedUseSelectorHook,
-} from "react-redux";
-import { RootState } from "./store";
-
-export const useSelector: TypedUseSelectorHook<RootState> = useReduxSelector;
->>>>>>> 0185803b3109c0c839bfdeecb21dcc88507afbd1
